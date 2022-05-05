18cb4747a77d74be7b6886119ce535b740857517
Dell <Dell@DESKTOP-DSTVUKM>
2022-05-06 02:49:57 +0300

    Second Commit

--- springit/src/main/java/com/vega/springit/SpringitApplication.java
+++ springit/src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,11 +1,21 @@
 package com.vega.springit;
 
+import org.aspectj.weaver.patterns.ArgsAnnotationPointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringitApplication {
-
+	
+	private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
